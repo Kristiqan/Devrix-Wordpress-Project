91a36c9fea5a4124c1c0b98cb7956d339a83dcde
git config --global user.name <kstefanov@devrix.com>
2022-08-18 19:16:23 +0300

    Added Filters to functions.php

--- wp-content/themes/wpdemo/functions.php
+++ wp-content/themes/wpdemo/functions.php
@@ -14,3 +14,59 @@ function my_theme_enqueue_styles() {
     );
 }
 ?>
+
+<?php
+add_filter('the_content', 'my_filter');
+function my_filter($content){
+  
+    if(is_singular()){
+    _e( 'This is my filter', 'hello' );
+   }
+}
+
+function div_one($content){
+  
+    if(is_singular()){
+    echo '<div>One</div>';
+   }
+}
+add_filter('the_content', 'div_one', 3);
+
+
+
+
+
+function div_two($content){
+  
+    if(is_singular()){
+    echo '<div>Two</div>';
+   }
+}
+add_filter('the_content', 'div_two', 2);
+
+
+function div_three($content){
+  
+    if(is_singular()){
+    echo '<div>Three</div>';
+   }
+}
+add_filter('the_content', 'div_three', 1);
+
+
+function members_only($items, $args ){
+     
+       if(is_user_logged_in()){
+        $items .=  '<li><a href="' . admin_url( 'profile.php' ) . '">Profile settings</a></li>';
+    }
+    return $items;
+        
+     
+}
+add_filter('wp_nav_menu_items', 'members_only', 10, 2)
+
+
+
+
+
+?>

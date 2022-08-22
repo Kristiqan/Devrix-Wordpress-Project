939ae5674abe379cc17b89fdd252fe84940f42cf
git config --global user.name <kstefanov@devrix.com>
2022-08-22 14:50:59 +0300

    Added wordpress tags in loop  get_header();, the_title(); etc.

--- wp-content/themes/wpdemo/My-Custom-Template.php
+++ wp-content/themes/wpdemo/My-Custom-Template.php
@@ -2,20 +2,39 @@
 /**
 * Template Name: My-Custom-Template
 * 
-*Template Post Type: post, page
+* Template Post Type: post, page
 *
 * @package WordPress
 * @subpackage Twenty_Twenty
 * @since Twenty Fourteen 1.0
 */
 
-get_header();
 
-echo '<ol><li>Title</li>
-<li>Featured Image</li>
-<li>Content</li>
-<li>The author</li></ol>';
 
-get_footer();
+
+$args = array (
+  
+'post_type' => 'post',
+'post_status'=> 'publish',
+'posts_per_page' => 1
+
+);
+
+$loop= new WP_Query($args);
+
+if ( $loop -> have_posts() ) :
+    while ( $loop -> have_posts() ) : $loop -> the_post();
+      get_header();
+       the_title();
+       the_post_thumbnail();
+        the_content();
+        the_author();
+        get_footer();
+
+endwhile;
+    endif;
+   
+
 ?>
 
+<?php   wp_reset_postdata();  ?>

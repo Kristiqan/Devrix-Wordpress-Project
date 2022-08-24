0389e485ed58b3e0ba4bd98c769b418f945744ab
Kristiqan Stefanov <kstefanov@devrix.com>
2022-08-24 13:37:21 +0300

    Tryed to add pagination

--- wp-content/themes/wpdemo/archive-students.php
+++ wp-content/themes/wpdemo/archive-students.php
@@ -10,35 +10,60 @@
  */
 
 get_header();
-
-
-$args = array (
-
-'post_type' => 'students',
-'post_status'=> 'publish',
-'posts_per_page' => 4
-
-);
-
-$loop= new WP_Query($args);
-
-if ( $loop->have_posts() ) :
-    while ( $loop->have_posts() ) : $loop->the_post();
-
-    
-       the_post_thumbnail();
-       the_title();
-       the_excerpt();
-        
-    
-        
-
-endwhile;
-    endif;
-
-
-    get_footer();
 ?>
+
+<main id="site-content">
+
+    <?php
+      $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+     $args = array(
+          
+        'post_type' => 'students',
+        'posts_per_page' => 4,
+        'paged' => $paged,
+        );
+
+        $loop= new WP_Query($args);
+
+      if ( $loop->have_posts() ) :
+
+        while ( $loop->have_posts() ) : $loop->the_post();
+            
+
+            the_post_thumbnail();
+            the_title();
+            the_excerpt();
+           
+            
+            
+            
+        endwhile;
+      endif;
+           
+      $big = 999999999;
+      echo paginate_links( array(
+           'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+           'format' => '?paged=%#%',
+           'current' => max( 1, get_query_var('paged') ),
+           'total' => $loop->max_num_pages,
+           'prev_text' => '&laquo;',
+           'next_text' => '&raquo;'
+      ) );
+      
+
+    ?>
+
+
+</div>
+</main>
+
+<?php the_posts_pagination( array( 'mid_size' => 6 ) ); ?>
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
+
 
 
 

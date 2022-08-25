f854bda7c6f3f1c1bec8dd6133916669bfba17fe
Kristiqan Stefanov <kstefanov@devrix.com>
2022-08-25 17:30:35 +0300

    Fixed add_meta_box but need help with checkbox

--- wp-content/themes/wpdemo/functions.php
+++ wp-content/themes/wpdemo/functions.php
@@ -119,108 +119,89 @@ function send_email_to_admin( $user_id, $old_data, $new_data ) {
 
 
 function dx_location_box() {
-    $screens = [ 'students' ];
-    foreach ( $screens as $screen ) {
-        add_meta_box(
-            'dxloc_box_id',                 
-            'Location',     
-            'dx_lo_box_html',  
-            $screen                    
-        );
-    }
+       
+    add_meta_box( 'dxloc_box_id', __('Location','twentytwentychild'), 'dx_lo_box_html',  'students');
+                                
+    
 }
 add_action( 'add_meta_boxes', 'dx_location_box' );
 
 
 function dx_lo_box_html( $post ) {
 ?>
-<label for="dx_field">Lives in</label>
+<label for="dx_field"><?php esc_html_e('Lives in:', 'twentytwentychild')?></label>
 <input type="text" id="dx_lo" name="dx_lo" value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'dx_lo', true))?>">
 <?php
 }
 
 function dx_addr_custom_box() {
-    $screens = [ 'students' ];
-    foreach ( $screens as $screen ) {
-        add_meta_box(
-            'dxaddr_box_id',                 
-            'Full Address:',     
-            'dx_addr_box_html',  
-            $screen                    
-        );
-    }
+       
+    add_meta_box('dxaddr_box_id',  __('Full Address:','twentytwentychild'),  'dx_addr_box_html', 'students' );                  
+    
 }
 add_action( 'add_meta_boxes', 'dx_addr_custom_box' );
 
 
 function dx_addr_box_html( $post ) {
 ?>
-<label for="dx_field">Address</label>
+<label for="dx_field"><?php esc_html_e('Address:', 'twentytwentychild')?></label>
 <input type="text" id="dx_address" name="dx_address" value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'dx_address', true))?>">
 <?php
 }
 
 function dx_birth_custom_box() {
-    $screens = [ 'students' ];
-    foreach ( $screens as $screen ) {
-        add_meta_box(
-            'dxabi_box_id',                 
-            'Birth Date:',     
-            'dx_bi_box_html',  
-            $screen                    
-        );
-    }
+        
+    add_meta_box('dxabi_box_id', __('Birth Date:', 'twentytwentychild'),  'dx_bi_box_html', 'students' );
+    
 }
 add_action( 'add_meta_boxes', 'dx_birth_custom_box' );
 
 
 function dx_bi_box_html( $post ) {
 ?>
-<label for="dx_field">Born</label>
+<label for="dx_field"><?php esc_html_e('Born:', 'twentytwentychild')?></label>
 <input type="date" id="dx_bi" name="dx_bi" value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'dx_bi', true))?>">
 <?php
 }
 
 function dx_cg_custom_box() {
-    $screens = [ 'students' ];
-    foreach ( $screens as $screen ) {
-        add_meta_box(
-            'dxcg_box_id',                 
-            'Class/Grade:',     
-            'dx_cg_box_html',  
-            $screen                    
-        );
-    }
+       
+    add_meta_box( 'dxcg_box_id', __('Class/Grade:', 'twentytwentychild'), 'dx_cg_box_html', 'students' );
+    
 }
 add_action( 'add_meta_boxes', 'dx_cg_custom_box' );
 
 
 function dx_cg_box_html( $post ) {
 ?>
-<label for="dx_field">Class And Grade</label>
+<label for="dx_field"><?php esc_html_e('Class And Grade:', 'twentytwentychild')?></label>
 <input type="text" id="dx_cg" name="dx_cg" value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'dx_cg', true))?>">
 <?php
 }
 
 
 function dx_acin_custom_box() {
-    $screens = [ 'students' ];
-    foreach ( $screens as $screen ) {
-        add_meta_box(
-            'dxacin_box_id',                 
-            'Active/inactive',     
-            'dx_acin_box_html',  
-            $screen                    
-        );
-    }
+
+    add_meta_box( 'dxacin_box_id', __('Active/inactive', 'twentytwentychild'), 'dx_acin_box_html', 'students');
+
 }
 add_action( 'add_meta_boxes', 'dx_acin_custom_box' );
 
 
 function dx_acin_box_html( $post ) {
+
+
+    $checkacin = esc_attr(get_post_meta(get_the_ID(), 'dx_acin', true));
+    $checked = 'checked';
+    if($checkacin == 'unchecked' && !empty($checkacin))
+    {   
+        $checked = ''; 
+    }
+
+
 ?>
-<label for="dx_field">Is the student active or not</label>
-<input type="text" id="dx_axin" name="dx_acin" value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'dx_acin', true))?>">
+<label for="dx_field"><?php esc_html_e('Is the student active or not', 'twentytwentychild')?></label>
+<input type="checkbox" id="dx_axin" name="dx_acin" value="1' <?php echo $checked;  ?> />
 <?php
 }
 
@@ -243,9 +224,15 @@ foreach ( $field_list as $fieldName){
         update_post_meta(
         $post_id,
         $fieldName,
-        sanitize_text_field( $_POST[ $fieldName ] )
-
-
+        sanitize_text_field( $_POST[ $fieldName ] ),
+        'dx_acin',
+        );
+    
+    }else{
+        update_post_meta(
+          $post_id,
+          'dx_acin',
+          'unchecked'
         );
     }
 }
